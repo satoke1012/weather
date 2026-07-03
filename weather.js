9fbb22a5596710dd3214171d269a472ac0837ecc
satoke1012 <k.sato@gunma-doujinkai.or.jp>
2026-07-03 15:39:16 +0900

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,1 +1,69 @@
+/*
+==========================================
+ 太田市 天気サイネージ Ver.5
+ 気象庁データ取得
+==========================================
+*/
 
+
+/*
+==========================================
+ 天気取得
+==========================================
+*/
+
+async function loadWeather(){
+
+    try{
+
+        const response = await fetch(
+
+            CONFIG.FORECAST_URL,
+
+            {
+                cache:"no-cache"
+            }
+
+        );
+
+        if(!response.ok){
+
+            throw new Error(
+
+                "HTTP Error : " + response.status
+
+            );
+
+        }
+
+        const json = await response.json();
+
+        saveCache(json);
+
+        parseWeather(json);
+
+    }
+
+    catch(error){
+
+        console.error(error);
+
+        console.log("キャッシュを使用します");
+
+        const cache = loadCache();
+
+        if(cache){
+
+            parseWeather(cache.data);
+
+        }
+
+        else{
+
+            showError();
+
+        }
+
+    }
+
+}
